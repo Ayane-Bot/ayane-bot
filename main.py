24892456c66e32959d524c6d1028148fb4035976
Leonardo Cavenago <leonardocavenago05@gmail.com>
2021-12-13 15:40:25 -0500

    updated intents

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import logging
-import discord
 
+import discord
 from discord.ext import commands
 
 from private.config import TOKEN, OWNER_IDS, DEFAULT_PREFIXES, LOCAL
@@ -9,13 +9,16 @@ from private.config import TOKEN, OWNER_IDS, DEFAULT_PREFIXES, LOCAL
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="[%(asctime)-15s] %(message)s")
 
+os.environ['JISHAKU_NO_UNDERSCORE'] = 'True'
+os.environ['JISHAKU_HIDE'] = 'True'
+
 
 class Ayane(commands.Bot):
     def __init__(self):
         super().__init__(
             command_prefix=commands.when_mentioned_or(*DEFAULT_PREFIXES),
             strip_after_prefix=True,
-            intents=discord.intents.all(),
+            intents=discord.Intents.all(),
         )
         self._load_cogs()
         self.owner_ids = OWNER_IDS
