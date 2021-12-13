e591d60510f980b88aa16089daf0d95deb95c421
ayane <ayane@waifu.im>
2021-12-13 20:38:06 +0000

    update

--- cogs/info.py
+++ cogs/info.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import datetime
 
 
 def setup(bot):
@@ -9,9 +10,70 @@ class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='info', aliases=['about'])
+    @commands.command(name="info", aliases=["about"])
     async def info(self, ctx):
         """
         Displays information about the bot.
         """
-        await ctx.send(f'Hello, I am a bot made by `LeoCx1000#9999`, `Buco#1169` and `veryon#1741`!')
+        await ctx.send(
+            f"Hello, I am a bot made by `LeoCx1000#9999`, `Buco#1169` and `veryon#1741`!"
+        )
+
+    @commands.command(aliases=["info"])
+    async def about(self, ctx):
+        """Some information about the bot, the bot owners, statistics, OS."""
+        text_channel = 0
+        voice_channel = 0
+        stage_channel = 0
+        for channel in self.bot.get_all_channels():
+            if isinstance(channel, discord.TextChannel):
+                text_channel += 1
+            elif isinstance(channel, discord.VoiceChannel):
+                voice_channel += 1
+            elif isinstance(channel, discord.StageChannel):
+                stage_channel += 1
+        bot_users = len(self.bot.users)
+        command_count = len(self.bot.commands)
+        guilds = len(self.bot.guilds)
+        owners_string = " ".join([f"`{own}`" for own in self.bot.owner_ids])
+        embed = discord.Embed(
+            title="Information about the bot",
+            color=self.bot.mycolor,
+        )
+        embed.set_thumbnail(url=owner.avatar.url)
+        embed.add_field(
+            name="<:bot_owner:846407210493804575> Owners",
+            value=f"Owners : {owners_string}\n",
+            inline=False,
+        )
+        embed.add_field(
+            name="<:stats:846407087491121224> Statistics",
+            value=f"""
+<:servers:846407428152492122> Servers : `{guilds}`
+<:users:846407378047729676> Users : `{bot_users}`
+<:voice_channel:846407273718743080> Voice channels : `{voice_channel}`
+<:text_channel:846407318982885435> Text channels : `{text_channel}`
+<:stage_channel:846410090050879529> Stage channels : `{stage_channel}`
+<:bot_commands:846415723798462464> Commands : `{command_count}`""",
+            inline=False,
+        )
+        embed.add_field(name="OS", value=f"OS : `{platform.system()}`", inline=True)
+        embed.add_field(
+            name="Versions",
+            value=f"<:python:846407588878876683> Python : `{platform.python_version()}`\n<:discordpy:846407533588381756> Discord.py : `{discord.__version__}`",
+            inline=False,
+        )
+        embed.add_field(
+            name="Links",
+            value=f"""
+[Bot Invite]({self.bot.invite})
+[Support Server]({self.bot.server_invite})
+[API Documentation](https://waifu.im/docs/)
+[Website]({self.bot.website})""",
+            inline=False,
+        )
+        embed.set_footer(
+            text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url
+        )
+        embed.timestamp = datetime.datetime.utcnow()
+        message = await ctx.send(embed=embed)
--- main.py
+++ main.py
@@ -7,37 +7,44 @@ from discord.ext import commands
 from private.config import TOKEN, OWNER_IDS, DEFAULT_PREFIXES, LOCAL
 
 log = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format='[%(asctime)-15s] %(message)s')
+logging.basicConfig(level=logging.INFO, format="[%(asctime)-15s] %(message)s")
 
 
 class Ayane(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or(*DEFAULT_PREFIXES), strip_after_prefix=True)
+        super().__init__(
+            command_prefix=commands.when_mentioned_or(*DEFAULT_PREFIXES),
+            strip_after_prefix=True,
+            intents=discord.intents.all(),
+        )
         self._load_cogs()
         self.owner_ids = OWNER_IDS
+        self.website = "https://ayane.live/"
+        self.invite = ""
+        self.server_invite = "https://discord.gg/QNXC8yFfKg"
 
     async def on_ready(self):
-        print('Logged in as', str(self.user))
+        print("Logged in as", str(self.user))
 
     def _load_cogs(self):
         try:
-            self.load_extension('jishaku')
+            self.load_extension("jishaku")
         except Exception as e:
-            logging.error(f'Failed to load jishaku', exc_info=e)
-        for ext in os.listdir('./cogs'):
-            if ext.endswith('.py'):
+            logging.error(f"Failed to load jishaku", exc_info=e)
+        for ext in os.listdir("./cogs"):
+            if ext.endswith(".py"):
                 try:
-                    self.load_extension(f'cogs.{ext[:-3]}')
+                    self.load_extension(f"cogs.{ext[:-3]}")
                 except Exception as e:
-                    logging.error(f'Failed to load extension {ext}.', exc_info=e)
+                    logging.error(f"Failed to load extension {ext}.", exc_info=e)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot = Ayane()
 
     @bot.check
     async def running_locally(ctx):
-        """ If the bot is running locally, only allows the owner to use commands. """
+        """If the bot is running locally, only allows the owner to use commands."""
         if LOCAL is False:
             return True
         return await bot.is_owner(ctx.author)
