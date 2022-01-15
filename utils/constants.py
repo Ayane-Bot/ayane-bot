6cc240da9e7643b5727ef024b168fd15afb10096
Buco <80671351+Bucolo@users.noreply.github.com>
2022-01-15 01:27:50 +0100

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,5 @@
 from waifuim import APIBaseURL
 
-APIDomainName = APIBaseURL.replace("https://api.","")
+APIDomainName = APIBaseURL.replace("https://api.","").rstrip("/")
 website = "https://ayane.live/"
 server_invite = "https://discord.gg/QNXC8yFfKg"
