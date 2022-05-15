958e3e012f9cfce07076d7b2b516a50283a48e31
Bucolo <arnaud@grimbert.com>
2022-05-15 15:03:58 +0200

    fix code error

--- cogs/waifu.py
+++ cogs/waifu.py
@@ -258,7 +258,8 @@ class Waifu(commands.Cog):
     @app_commands.autocomplete(tag=sfw_tag_autocomplete, order_by=order_by_autocomplete)
     async def sfw_(self, interaction, tag: str, order_by: str = None, gif: bool = None, many: bool = None):
         if tag.lower() not in interaction.client.waifu_im_tags['sfw']:
-            return await interaction.response.send_message(f"Please choose a tag of the followings: {', '.join(['``'+t+'`' for t in interaction.client.waifu_im_tags['sfw']])}")
+            tags_string = ', '.join(['`' + t + '`' for t in interaction.client.waifu_im_tags['sfw']])
+            return await interaction.response.send_message("Please choose a tag of the followings: " + tags_string)
         await self.waifu_launcher(
             interaction,
             is_nsfw=False,
