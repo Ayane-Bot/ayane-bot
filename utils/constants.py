d5a11083013b60033d9a698dc4f0d4fd649068bb
Bucolo <arnaud@grimbert.com>
2022-06-12 14:23:34 +0200

    change site domain

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,5 @@
 from waifuim import APIBaseURL
 
 APIDomainName = APIBaseURL.replace("https://api.","").rstrip("/")
-website = "https://ayane.live/"
+website = "https://ayane.fr/"
 server_invite = "https://discord.gg/QNXC8yFfKg"
